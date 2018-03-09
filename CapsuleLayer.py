f06616cebbe32c5a2111ec30beee1a6987dcb95a
Brenton Chu <brentonlongchu@berkeley.edu>
2018-03-09 13:37:12 -0800

    some reformatting

--- CapsuleLayer.py
+++ CapsuleLayer.py
@@ -1,33 +1,32 @@
 class CapsuleLayer(nn.Module):
-	def __init__(self,in_capsules,in_channels,out_capsules,unit_size,use_routing,num_iters,use_cuda,filter_size=9,stride=2):
-		super(CapsuleLayer, self).__init__()
-		self.in_capsules = in_capsules
-		self.in_channels = in_channels
-		self.out_capsules = out_capsules
-		self.unit_size = unit_size
-		self.use_routing = use_routing
-		self.num_iters = num_iters
-		self.use_cuda = use_cuda
-		self.filter_size = filter_size
-		self.stride = stride
+    def __init__(self, in_capsules, in_channels, out_capsules, unit_size, use_routing, num_iters,
+                 use_cuda, filter_size=9, stride=2):
+        super(CapsuleLayer, self).__init__()
+        self.in_capsules = in_capsules
+        self.in_channels = in_channels
+        self.out_capsules = out_capsules
+        self.unit_size = unit_size
+        self.use_routing = use_routing
+        self.num_iters = num_iters
+        self.use_cuda = use_cuda
+        self.filter_size = filter_size
+        self.stride = stride
 
-		if self.use_routing:
-			#do routing
-		else:
-			self.conv_capsules = nn.ModuleList([nn.Conv2d(self.in_channels,self.unit_size,self.filter_size,stride) for 
-				unit in range(self.out_capsules)])
+        if self.use_routing:
+            #do routing
+        else:
+            conv_list = [nn.Conv2d(self.in_channels, self.unit_size, self.filter_size, stride)
+                         for unit in range(self.out_capsules)]
+            self.conv_capsules = nn.ModuleList(conv_list)
 
-	def forward(self,x):
-		#x=[batch_size,in_channels,width,height]
-		capsules = [conv(x) for _,conv in enumerate(self.conv_capsules)]
+    def forward(self, x):
+        #x=[batch_size,in_channels,width,height]
+        capsules = [conv(x) for _,conv in enumerate(self.conv_capsules)]
+        capsules = torch.stack(capsules, dim = 1)
+        capsules = capsules.view(x.size(0),self.out_capsules,-1)
+        return self.squash(capsules,dim=2)
 
-		capsules = torch.stack(capsules, dim = 1)
-
-		capsules = capsules.view(x.size(0),self.out_capsules,-1)
-
-		return self.squash(capsules,dim=2)
-
-	def squash(vec,dim=2):
-		norm = torch.norm(vec,dim,keepdim=True)
-		return vec*norm/(1+norm**2)
+    def squash(vec, dim=2):
+        norm = torch.norm(vec,dim,keepdim=True)
+        return vec*norm/(1+norm**2)
 
--- model.py
+++ model.py
@@ -5,9 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                         download=True, transform=transform)
@@ -22,27 +21,23 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
-
-
-
 class CapsuleNet(nn.Module):
-	def __init__(self):
-		super(CapsuleNet,self).__init__()
-		self.conv1 = nn.Conv2d(in_channels=3,out_channels=256,kernel_size=9,stride=1)
-		self.primary_capsules()
-		self.class_capsules()
+    def __init__(self):
+        super(CapsuleNet,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3,out_channels=256,kernel_size=9,stride=1)
+        self.primary_capsules()
+        self.class_capsules()
 
-	def forward(self,x,y=None):
-		#x = [batch_size,3,32,32]
-		#y = [batch_size,10]
+    def forward(self,x,y=None):
+        #x = [batch_size,3,32,32]
+        #y = [batch_size,10]
 
-		x = F.relu(self.conv1(x))
-		x = self.primary_capsules(x)
-		x = self.class_capsules(x)
-		return x
+        x = F.relu(self.conv1(x))
+        x = self.primary_capsules(x)
+        x = self.class_capsules(x)
+        return x
 
-	def loss()
+    def loss()
 
 
 
